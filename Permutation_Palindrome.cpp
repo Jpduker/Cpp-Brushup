2e4acfe81632095fddc56df7da7527d24de98cd8
Jp <jayaprakashnis619@gmail.com>
2022-08-31 22:50:02 +0530

    com

--- Permutation_Palindrome.cpp
+++ Permutation_Palindrome.cpp
@@ -5,12 +5,24 @@ class Solution
 {
 public:
     string str;
-    void palindrome()
+    void palindrome(string str)
     {
         int char_counts[128] = {0};
         for (int i; i < str.length(); ++i)
         {
             char_counts[str.at(i)]++;
         }
+        int count = 0;
+        for (int i; i < 128; i++)
+        {
+            count += char_counts[i] % 2;
+        }
+        (count <= 1) ? cout << "true" : cout << "false";
     }
 };
+
+int main()
+{
+    Solution pal;
+    pal.palindrome("rac");
+}
