2d14890bfbe0cf9e6dd92e7528a9ac6242a82005
Jp <jayaprakashnis619@gmail.com>
2022-09-06 18:39:17 +0530

    Implemented Insert Fromt LL

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -23,5 +23,24 @@ public:
         Node *newNode = new Node();
         newNode->data = data;
         newNode->next = NULL;
+        if (Head == NULL)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            newNode->next = Head;
+            Head = newNode;
+        }
     }
 };
+
+int main()
+{
+    LindedList lst;
+    lst.insertNodeFront(20);
+    lst.insertNodeFront(30);
+    lst.insertNodeFront(40);
+    lst.insertNodeFront(50);
+}
